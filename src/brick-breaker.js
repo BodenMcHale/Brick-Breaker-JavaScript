99ff7a799558e7c9e05a57d163e0bd91b797ae6b
Boden McHale <bodenkaikarden@gmail.com>
2022-06-17 12:08:39 -0400

    Update brick-breaker.js

--- src/brick-breaker.js
+++ src/brick-breaker.js
@@ -1,9 +1,37 @@
-// Set random angle on start
-// Round edges of boxes
-// Bug if the ball goes under the paddle it'll come back up
-// Add header file
-// Add another screenshot to github
-// Update github to include audio in readme
+/*
+    License
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    MIT License
+    Copyright (c) 2022, Boden McHale
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+	
+    Future Modifications
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    - Random angle on start
+    - More sfx
+	- Round edges of rectangles
+	- Bug: If the ball goes under the paddle it pops back up
+
+    Author
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Twitter: @Boden_McHale https://twitter.com/Boden_McHale
+    Blog: https://lostrabbitdigital.com/blog/
+    Last Updated: June 16th 2022
+*/
 
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
